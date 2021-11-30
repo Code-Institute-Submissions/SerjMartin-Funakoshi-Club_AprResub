e709997a0f200b3f3c46f265b7ecf506cb14963c
SerjMartin <serjmartin@mail.com>
2021-11-30 18:15:51 +0000

    Install summenrnote library

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+# Import post model from models.py
+from . models import Post
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Post)
+class PostAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('content')
