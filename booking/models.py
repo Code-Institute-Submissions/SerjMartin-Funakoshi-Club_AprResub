0dc6e0ab0f90e909cb366bdd7f46eaba021bd2ad
SerjMartin <serjmartin@mail.com>
2021-12-02 21:19:41 +0000

    Create booking model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Booking(models.Model):
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created_on']
+
+    def __str__(self):
+        return f"{self.name} your place successful booked"
