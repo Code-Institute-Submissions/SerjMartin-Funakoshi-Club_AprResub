11e5620498105380625ae45e7bd42b646993bbbb
SerjMartin <serjmartin@mail.com>
2021-12-02 22:00:45 +0000

    Add Booking model to the admin site

--- booking/models.py
+++ booking/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+STATUS = ((0, "Draft"), (1, "Booked"))
+
 
 class Booking(models.Model):
     name = models.CharField(max_length=80)
     email = models.EmailField()
+    phone_number = models.CharField(max_length=50)
     created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
 
     class Meta:
         ordering = ['created_on']
