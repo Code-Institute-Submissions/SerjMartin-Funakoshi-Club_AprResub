7b2aa3426aa5197d7eb220670fddbf6b03efbf91
SerjMartin <serjmartin@mail.com>
2021-12-25 20:23:33 +0000

    Deployment

--- funakoshi/settings.py
+++ funakoshi/settings.py
@@ -34,7 +34,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEVELOPMENT')
+DEBUG = False
+
+X_FRAME_OPTION = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['funakoshiclub.herokuapp.com', 'localhost']
 # 'funakoshiclub.herokuapp.com', 'localhost'
