04e9fe3ede78168e4e5323c9d94652e5bfdd242d
SerjMartin <serjmartin@mail.com>
2022-04-05 16:31:31 +0000

    Fix pep 8 code validations

--- blog/models.py
+++ blog/models.py
@@ -16,7 +16,8 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_post")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
@@ -40,13 +41,15 @@ class Post(models.Model):
 # Add Comment model
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=True)
-    likes = models.ManyToManyField(User, related_name='comment_likes', blank=True)
+    likes = models.ManyToManyField(
+        User, related_name='comment_likes', blank=True)
 
     class Meta:
         ordering = ['created_on']
