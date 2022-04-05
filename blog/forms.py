4e0bc55b6a6acaef4101b52209bba323e263dadc
SerjMartin <serjmartin@mail.com>
2022-04-05 16:30:40 +0000

    Add textarea widget to forms

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,10 @@
 from .models import Comment
 from django import forms
 
+
 class CommentForm(forms.ModelForm):
+    body = forms.CharField(widget=forms.Textarea)
+
     class Meta:
         model = Comment
         fields = ('body',)
